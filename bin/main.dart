c1987d9f75a0d3bb91aec3a932544d8adb79e224
Skylled <Kyle@hotfixit.net>
2018-04-23 11:02:49 -0500

    Add Account caching

--- bin/main.dart
+++ bin/main.dart
@@ -5,6 +5,10 @@ import 'package:http/http.dart';
 
 bool _debugging = false;
 
+Map<String, Map<String, dynamic>> cachedAccounts;
+bool cacheLoaded = false;
+File accountsCacheFile;
+
 // Reference guide: https://flutter.io/networking/#example-decoding-json-from-https-get
 
 // Future: Generate XSRF_TOKEN legitimately.
@@ -22,13 +26,28 @@ String makeCookie(){
   return null;
 }
 
+void loadAccountCache(){
+  accountsCacheFile = new File('cache/accounts.json');
+  Map decJson = json.decode(accountsCacheFile.readAsStringSync());
+  cachedAccounts = decJson.cast<String, Map<String, dynamic>>();
+  cacheLoaded = true;
+}
+
 // Future: de-dup code from these.
-Future<Map<String, dynamic>> scrapeAccount(String accountID) async {
+Future<Map<String, dynamic>> scrapeAccount(String accountID, {cached = true}) async {
+  if (!cacheLoaded)
+    loadAccountCache();
+  if (cached && cachedAccounts.containsKey(accountID)) {
+    return cachedAccounts[accountID];
+  }
   // print('Scraping for account: $accountID');
   Response res = await get('https://fuchsia-review.googlesource.com/accounts/$accountID', headers: {'Cookie': makeCookie()});
   if (res.statusCode == HttpStatus.OK) {
     Map decJson = json.decode(res.body.substring(5));
-    return decJson.cast<String, dynamic>();
+    Map<String, dynamic> newAccount = decJson.cast<String, dynamic>();
+    cachedAccounts[accountID] = newAccount;
+    accountsCacheFile.writeAsStringSync(json.encode(cachedAccounts));
+    return newAccount;
   } else {
     throw new HttpException('Status code: ${res.statusCode}');
   }
